6188650d8c93ec7e0e29d8e8df33d9e243cb5e21
jojoldu2daum <jojoldu@daum.net>
2021-09-08 16:27:28 +0900

    4장 완성

--- src/main/java/com/koscom/springboot/service/PostsService.java
+++ src/main/java/com/koscom/springboot/service/PostsService.java
@@ -2,12 +2,17 @@ package com.koscom.springboot.service;
 
 import com.koscom.springboot.domain.posts.Posts;
 import com.koscom.springboot.domain.posts.PostsRepository;
+import com.koscom.springboot.web.dto.posts.PostsListResponseDto;
 import com.koscom.springboot.web.dto.posts.PostsResponseDto;
 import com.koscom.springboot.web.dto.posts.PostsSaveRequestDto;
 import com.koscom.springboot.web.dto.posts.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final로 선언된 필드들은 생성자 항목으로 자동 포함시켜서 생성자 생성
 @Service // spring bean 등록 & Service 클래스 선언
@@ -41,5 +46,31 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자는 없습니다. id=" +id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        // java 7까지 방식
+//        List<Posts> allDesc = postsRepository.findAllDesc();
+//        List<PostsListResponseDto> result = new ArrayList<>();
+//
+//        for (Posts posts : allDesc) {
+//            result.add(new PostsListResponseDto(posts));
+//        }
+//
+//        return result;
+
+        // java 8
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // 치환
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
